76ae0993c3b540766ce039c02cf996b60cd1118a
Dopesky <oboke69@gmail.com>
2018-09-30 04:04:38 +0300

    Added model class to connect to db and retrieve/send log info

---
+++ SMSBank/application/models/GetLog.php
@@ -0,0 +1,28 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class GetLog extends CI_Model {
+	
+	public function __construct(){
+		parent::__construct();
+	}
+
+	public function index(){
+		$this->db->select('log.Log_ID,fJoin.Name as tName,accounts.Name as rName,log.Transactions,log.Amount,log.Time');
+		$this->db->from('log');
+		$this->db->join('accounts fJoin', 'log.Acc_No = fJoin.Acc_No','left');
+		$this->db->join('accounts', "log.`To/From`=accounts.Acc_No",'left');
+		return $this->db->get()->result();
+	}
+
+	public function log_transaction($source,$transaction,$amount,$recipient="N/A"){
+		if($recipient==="N/A"){
+			$recipient=$source;
+		}
+		$this->db->select('max(Log_ID) as maxID');
+		$this->db->from('log');
+		$logID=$this->db->get()->row()->maxID+1;
+		$data=array('Log_ID'=>$logID,'Acc_No'=>$source,'`To/From`'=>$recipient,'Transactions'=>$transaction,'Amount'=>$amount);
+		return $this->db->insert("log",$data,true);
+	}
+}
