a8a7d4e7e3ef370e12e9c7d124243f1034714870
Dopesky <oboke69@gmail.com>
2018-09-30 15:51:19 +0300

    Added function to get the unique transaction log for specific users

--- SMSBank/application/models/GetLog.php
+++ SMSBank/application/models/GetLog.php
@@ -25,4 +25,13 @@ class GetLog extends CI_Model {
 		$data=array('Log_ID'=>$logID,'Acc_No'=>$source,'`To/From`'=>$recipient,'Transactions'=>$transaction,'Amount'=>$amount);
 		return $this->db->insert("log",$data,true);
 	}
+
+	public function get_unique($acc_no){
+		$this->db->select('log.Log_ID,fJoin.Name as tName,accounts.Name as rName,log.Transactions,log.Amount,log.Time');
+		$this->db->from('log');
+		$this->db->join('accounts fJoin', 'log.Acc_No = fJoin.Acc_No','left');
+		$this->db->join('accounts', "log.`To/From`=accounts.Acc_No",'left');
+		$this->db->where("log.Acc_No=$acc_no or log.`To/From`=$acc_no");
+		return $this->db->get()->result();
+	}
 }
