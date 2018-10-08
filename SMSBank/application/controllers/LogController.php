91284e7748a5536fa18bbeae1727a6d57470cd02
Dopesky <oboke69@gmail.com>
2018-10-08 11:59:34 +0300

    Added a function to get the specific log details for a specific person

--- SMSBank/application/controllers/LogController.php
+++ SMSBank/application/controllers/LogController.php
@@ -12,6 +12,18 @@ class LogController extends CI_Controller {
 		$data['t_log']=$this->getlog->index();
 		$data['links']=$this->loadscripts->index();
 		$data['datatable']=$this->loadscripts->datatable();
-		$this->load->view('showlog',$data);
+		$this->load->view('show_log',$data);
+	}
+
+	public function get_unique_log(){
+		if($this->session->userdata('accountnumber')!==null){
+			$this->load->model('getlog');
+			$data['t_log']=$this->getlog->get_unique($this->session->userdata('accountnumber'));
+			$data['links']=$this->loadscripts->index();
+			$data['datatable']=$this->loadscripts->datatable();
+			$this->load->view('show_unique_log',$data);
+			return;
+		}
+		redirect("","location");
 	}
 }
