497670c03fd85e333155b1722fb00cb67bd094e1
dopesky <oboke69@gmail.com>
2018-10-15 00:24:37 +0300

    Changed base url

--- SMSBank/application/config/config.php
+++ SMSBank/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/HTML/Student-Monetary-System/SMSBank/';
+$config['base_url'] = 'http://localhost/MyProjects/Student-Monetary-System/SMSBank/';
 
 /*
 |--------------------------------------------------------------------------
