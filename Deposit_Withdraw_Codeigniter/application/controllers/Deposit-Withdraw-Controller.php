5c69b72678e18bbabbee28acf4f85f0b8cf1f132
BuruguEric <ericgburugu@gmail.com>
2018-10-20 14:23:28 +0300

    Updated Version

--- Deposit_Withdraw_Codeigniter/application/controllers/Deposit-Withdraw-Controller.php
+++ Deposit_Withdraw_Codeigniter/application/controllers/Deposit-Withdraw-Controller.php
@@ -6,9 +6,9 @@ class Deposit_Withdraw_Controller extends CI_Controller {
         $this->load->model('deposit_withdraw_model');
     	$success=$this->deposit_withdraw_model->deposit($_POST['amount'],$this->session->userdata('accountnumber'));
         if($success){
-
+            echo "Successful deposit of Ksh". $_POST['amount'] ." to account number ".$this->session->userdata('accountnumber').;
         }else{
-            
+            echo "Invalid account.\nThe account does not exist.";
         }
     }
     public function SMS_withdraw(){
--- Deposit_Withdraw_Codeigniter/application/models/Deposit-Withdraw-Model.php
+++ Deposit_Withdraw_Codeigniter/application/models/Deposit-Withdraw-Model.php
@@ -14,9 +14,34 @@ class Deposit_Withdraw_Model extends CI_Model {
         return $this->db->select('Balance')->where('acc_no',$accountNumber)->get('accounts')->row()->Balance;
     }
 
-    public function deposit($amount,$accountNumber){}
+    public function deposit($amount,$accountNumber){
+        $this->$amount += $balance;
+        return $amount;
+    }
 
-    public function withdraw($amount,$accountNumber){}
+    public function withdraw($amount,$accountNumber){
+        $this->$amount -= $balance;
+        return $amount;
+    }
 
-    public function transfer($amount,$to,$from){}
+    public function transfer($amount,$to,$from){
+        $this->db->get_userdata($accountNumber);
+        $this->db->execute(array('$accountNumber' => $from));
+        $this->availableAmount = $this->db->('Balance') -> fetchColumn();
+        if ($this->availableAmount < $amount) {
+            echo "Insufficient funds.\nUnable to complete transaction process.";
+            return false;
+        }else{
+            $this->$amount = $availableAmount - $amount;
+            $this->db->update_userdata($amount,$from);
+
+            $this->db->get_userdata($to);
+            $this->$balance = $this->db->('Balance') -> fetchColumn();
+            $this->amount += $balance;
+            $this->db->update_userdata($amount,$to);
+            $this->db->commit();
+        }
+        
+
+    }
 }
